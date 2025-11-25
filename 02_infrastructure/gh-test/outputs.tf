9f8a706d186893f04fcd404b85eed77d6e0b81e8
Hrvoje Kusanic <hrvoje.kusanic@unique.ai>
2025-11-25 09:10:15 +0100

    chore: disable github runners for now

--- 02_infrastructure/gh-test/outputs.tf
+++ 02_infrastructure/gh-test/outputs.tf
@@ -1,4 +1,5 @@
-output "network_settings_id" {
-  description = "ID of the GitHub.Network/networkSettings resource"
-  value       = azapi_resource.github_network_settings.output.tags.GitHubId
-}
+# TODO: to be cleaned up at later point in time
+# output "network_settings_id" {
+#   description = "ID of the GitHub.Network/networkSettings resource"
+#   value       = azapi_resource.github_network_settings.output.tags.GitHubId
+# }
