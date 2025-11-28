a958dacafba9d2c702602cd3b23c6d2ee85ff15c
Hrvoje Kusanic <hrvoje.kusanic@unique.ai>
2025-11-28 09:36:35 +0100

    chore: since there is a validation on var.env remove check in locals

--- 01_bootstrap/day-0/02-locals.tf
+++ 01_bootstrap/day-0/02-locals.tf
@@ -1,5 +1,5 @@
 locals {
-  resource_group_name = var.resource_group_name != null ? var.resource_group_name : "rg-terraform-state-${var.env}"
-  key                 = var.key != null ? var.key : "terraform-init-${var.env}-v2.tfstate"
+  resource_group_name = "rg-terraform-state-${var.env}"
+  key                 = "terraform-init-${var.env}-v2.tfstate"
 }
 
