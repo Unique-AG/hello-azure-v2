f1cfb4e3a06a04e93de712a076699ee42578a8a9
Jakub Kaleta <jakub.kaleta@unique.ai>
2025-12-08 12:44:46 +0100

    remove unrelated comment

--- 02_infrastructure/day-2/03-data.tf
+++ 02_infrastructure/day-2/03-data.tf
@@ -55,7 +55,6 @@ data "azurerm_virtual_network" "vnet_day_1" {
 }
 
 # Subnet data source for cognitive services (created in day-1)
-# This will be used if var.subnet_cognitive_services_id is not provided
 data "azurerm_subnet" "subnet_cognitive_services_day_1" {
   name                 = var.subnet_cognitive_services_id
   virtual_network_name = data.azurerm_virtual_network.vnet_day_1.name
@@ -63,7 +62,6 @@ data "azurerm_subnet" "subnet_cognitive_services_day_1" {
 }
 
 # Private DNS Zone data source for speech service (created in day-1)
-# This will be used if var.private_dns_zone_speech_service_id is not provided
 data "azurerm_private_dns_zone" "speech_service_day_1" {
   name                = var.speech_service_private_dns_zone_name
   resource_group_name = var.resource_group_name_vnet
