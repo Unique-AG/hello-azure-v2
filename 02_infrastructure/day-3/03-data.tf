a5bc5af89cbcac1cd944a3acdef46b7413946721
Hrvoje Kusanic <hrvoje.kusanic@unique.ai>
2026-04-02 11:26:29 +0200

    fix: narrow plan trigger to day-3 and remove unused data sources

--- 02_infrastructure/day-3/03-data.tf
+++ 02_infrastructure/day-3/03-data.tf
@@ -1,22 +1,5 @@
 # Data sources for referencing existing Azure resources from day-1
 
-data "azurerm_subscription" "current" {}
-
-# Resource Group data sources (created in day-1)
-data "azurerm_resource_group" "core" {
-  name = var.resource_group_core_name
-}
-
-data "azurerm_resource_group" "vnet" {
-  name = var.resource_group_name_vnet
-}
-
-# Virtual Network data source (created in day-1)
-data "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
-  resource_group_name = var.resource_group_name_vnet
-}
-
 # Subnet data source - AzureBastionSubnet (created in day-1)
 data "azurerm_subnet" "bastion" {
   name                 = "AzureBastionSubnet"
